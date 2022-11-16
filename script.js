b0aabb4d07ba5c76b2ff5bfdce632724d6f1bb47
Matej Bednar <matejbednar55@gmail.com>
2022-11-16 14:23:14 +0100

    Update styling, listen on movement, generate new box, reset game

--- script.js
+++ script.js
@@ -3,43 +3,57 @@ const boardElement = document.getElementById('board');
 const SIZE = 3;
 const BASE_VALUE = 3;
 
+const DIRECTION = {
+	UP: 'UP',
+	DOWN: 'DOWN',
+	LEFT: 'LEFT',
+	RIGHT: 'RIGHT'
+}
+
 const Board = {
-	desk: [],
+	_desk: [],
 	init: function() {
 		console.log('Board init');
 		for (let i = 0; i < SIZE; i++) {
-			this.desk[i] = [];
+			this._desk[i] = [];
 			for (let j = 0; j < SIZE; j++) {
 				const box = {
 					x: i,
 					y: j,
 					value: 0
 				};
-				this.desk[i][j] = box;
+				this._desk[i][j] = box;
 			}
 		}
 	},
 	generateBox: function () { // get one random tile from 2D array where value == 0 & set 3
-		const freeTiles = this.desk.flatMap(x => x).filter((box) => box.value === 0);
+		const freeTiles = this._desk.flatMap(x => x).filter((box) => box.value === 0);
+
+		if (freeTiles.length === 0) {
+			Game.lost();
+			return;
+		}
+
 		const selectedTile = freeTiles[Math.floor(Math.random()*freeTiles.length)];
+		this._desk[selectedTile.x][selectedTile.y].value = BASE_VALUE;
+		console.log(this._desk[selectedTile.x][selectedTile.y]);
 
-		this.desk[selectedTile.x][selectedTile.y].value = BASE_VALUE;
-		console.log(this.desk[selectedTile.x][selectedTile.y]);
-
-		Draw.drawBox(this.desk[selectedTile.x][selectedTile.y]);
+		Draw.drawBox(this._desk[selectedTile.x][selectedTile.y]);
 	},
 }
 
 
 const Draw = {
 	init: function () {
-		for (let i = 0; i < SIZE; i++) {
-			for (let j = 0; j < SIZE; j++) {
-				const box = document.createElement('div');
-				box.classList.add('box');
-				// boardElement.appendChild(box);
-			}
-		}
+		boardElement.innerHTML = "";
+
+		// for (let i = 0; i < SIZE; i++) {
+		// 	for (let j = 0; j < SIZE; j++) {
+		// 		const box = document.createElement('div');
+		// 		box.classList.add('box');
+		// 		// boardElement.appendChild(box);
+		// 	}
+		// }
 	},
 	calculatePixelPosition: function (x, y) {
 		const left = boardElement.getBoundingClientRect().width / SIZE * x;
@@ -51,7 +65,6 @@ const Draw = {
 		const pixelPosition = this.calculatePixelPosition(box.x, box.y);
 		// console.log(pixelPosition);
 		const boxElement = this.createBox(pixelPosition.x, pixelPosition.y);
-		// boxElement.innerHTML = box.value;
 
 		boardElement.appendChild(boxElement);
 
@@ -80,8 +93,10 @@ const Draw = {
 
 const Player = {
 	score: 0,
-	makeMove: function () {
+	makeMove: function (direction) {
+		console.log(direction);
 
+		Board.generateBox();
 	},
 	incrementScore: function (value) {
 		this.score += value;
@@ -93,22 +108,35 @@ const Player = {
 
 const Game = {
 
+	reset: function () {
+		Player.resetScore();
+		Board.init();
+		Draw.init();
+	},
+	lost: function () {
+		console.log('Game over');
+		this.reset();
+	},
+	win: function () {
+		console.log('You won');
+		this.reset();
+	}
 }
 
 document.onkeydown = function (e) {
 	switch (e.key) {
 		case "ArrowLeft":
-        // Left pressed
-        break;
+			Player.makeMove(DIRECTION.LEFT);
+        	break;
    		case "ArrowRight":
-        // Right pressed
-        break;
+			Player.makeMove(DIRECTION.RIGHT);
+	        break;
     	case "ArrowUp":
-        // Up pressed
-        break;
+			Player.makeMove(DIRECTION.UP);
+    	    break;
     	case "ArrowDown":
-        // Down pressed
-        break;
+			Player.makeMove(DIRECTION.DOWN);
+        	break;
 	}
 }
 
