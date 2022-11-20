e881577ae73563344d9e9650f640ea7ee8414ab5
Matej Bednar <matejbednar55@gmail.com>
2022-11-20 12:37:16 +0100

    Generate new box only if state was updated, format code

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const DIRECTION = {
 
 const Board = {
 	_desk: [],
+	_oldDesk: [],
 	init: function() {
 		console.log('Board init');
 		for (let i = 0; i < SIZE; i++) {
@@ -20,11 +21,26 @@ const Board = {
 				this._desk[i][j] = this.getEmptyBox(i, j);
 			}
 		}
+
+		this._saveOldState();
 	},
 	getDesk: function() {
 		return this._desk;
 	},
-	generateBox: function () { // urobime tu counter, ktory pocita kolko krat zasebou bol dany smer. Ak mame zasebou 2x dole, tak uz nebude generovat novy box - musi sa ist do inej strany
+	_saveOldState: function () {
+		this._oldDesk = JSON.parse(JSON.stringify(this._desk));
+	},
+	isMoved: function() {
+		for (let i = 0; i < SIZE; i++) {
+			for (let j = 0; j < SIZE; j++) {
+				if (this._desk[i][j].value !== this._oldDesk[i][j].value) {
+					return true;
+				}
+			}
+		}
+		return false;
+	},
+	generateBox: function () {
 		const freeTiles = this._desk.flatMap(x => x).filter((box) => box.value === 0);
 
 		if (freeTiles.length === 0) {
@@ -52,111 +68,37 @@ const Board = {
 	getEmptyBox: function (x, y) {
 		return {x, y, value: 0};
 	},
-	getHgfg: function (direction) {
-		switch (direction) {
-			case DIRECTION.LEFT:
-			case DIRECTION.UP:
-				return {start: 0, end: SIZE, kremes: 1}
-			case DIRECTION.RIGHT:
-			case DIRECTION.DOWN:
-				return {start: SIZE-1, end: -1, kremes: -1}
-		}
-	},
 	mergeBoxes: function (direction) {
-
-
-
-		// for (let i = 0; i < SIZE; i++) { // ak ideme Zhora dole, tak otocime pole?
-		// 	columnBoxes.push(this._desk[0][i]);
-		// }
-
-		// for (let s = 0; s < SIZE; s++) {
-		// 	const s = 0;
-
-		// 	// for (let i = hgfg.start; i > hgfg.end; i+=hgfg.kremes) {
-		// 	for (let i = hgfg.start; i > hgfg.end; i+=hgfg.kremes) {
-		// 		// if (direction === DIRECTION.UP || DIRECTION === DIRECTION.DOWN) {
-		// 			columnBoxes.push(this._desk[s][i]);
-		// 		// } else {
-		// 		// 	columnBoxes.push(this._desk[i][s]);
-		// 		// }
-		// 		// columnBoxes.push(this._desk[0][i]);
-		// 	}
-
-		// 	let filteredColumnBoxes = columnBoxes.filter(box => box.value !== 0);
-		// 	console.log(filteredColumnBoxes);
-		// 	for (let j = 0; j < filteredColumnBoxes.length - 1; j++) {
-		// 		if (filteredColumnBoxes[j].value === filteredColumnBoxes[j+1].value) {
-		// 			filteredColumnBoxes[j].value *= 2;
-		// 			filteredColumnBoxes.splice(j+1, 1);
-		// 			console.log(filteredColumnBoxes);
-		// 		}
-		// 	}
-
-		// 	if (filteredColumnBoxes.length) {
-		// 		for (let j = hgfg.start; j > hgfg.end; j+=hgfg.kremes) {
-		// 			if (j < filteredColumnBoxes.length) {
-		// 				// if (direction === DIRECTION.UP || DIRECTION === DIRECTION.DOWN) {
-		// 					this._desk[s][j].value = filteredColumnBoxes[j].value; // fix
-		// 				// } else {
-		// 				// 	this._desk[j][s].value = filteredColumnBoxes[j].value; // fix
-		// 				// }
-
-		// 				// this._desk[0][j].value = filteredColumnBoxes[j].value; // fix
-		// 			} else {
-		// 				// if (direction === DIRECTION.UP || DIRECTION === DIRECTION.DOWN) {
-		// 					this._desk[s][j].value = 0; // fix
-		// 				// } else {
-		// 				// 	this._desk[j][s].value = 0; // fix
-		// 				// }
-		// 				// this._desk[0][j].value = 0; // fix
-		// 			}
-		// 		}
-		// 		// for (let j = 0; j < SIZE; j++) {
-		// 		// 	if (j < filteredColumnBoxes.length) {
-		// 		// 		this._desk[0][j].value = filteredColumnBoxes[j].value; // fix
-		// 		// 	} else {
-		// 		// 		this._desk[0][j].value = 0; // fix
-		// 		// 	}
-		// 		// }
-		// 	}
-
-		// // }
-		// console.log(this._desk);
+		this._saveOldState();
 
 		for(let s = 0;s < SIZE; s++){
-			console.log(this._desk);
+			// console.log(this._desk);
 			const columnBoxes = [];
 
-			// const s = 0;
-
-			if(direction === DIRECTION.UP){
+			if (direction === DIRECTION.UP) {
 				for (let i = 0; i < SIZE; i++) {
 					columnBoxes.push(this._desk[s][i]);
 					console.log(this._desk[s][i]);
 				}
-			}else if(direction === DIRECTION.DOWN){
+			} else if (direction === DIRECTION.DOWN) {
 				for (let i = SIZE-1; i >= 0; i--) {
 					columnBoxes.push(this._desk[s][i]);
-
 				}
-			}else if(direction === DIRECTION.LEFT){
+			} else if (direction === DIRECTION.LEFT) {
 				for (let i = 0; i < SIZE; i++) {
 					columnBoxes.push(this._desk[i][s]);
 				}
-			}else if(direction === DIRECTION.RIGHT){
+			} else if (direction === DIRECTION.RIGHT) {
 				for (let i = SIZE-1; i >= 0; i--) {
 					columnBoxes.push(this._desk[i][s]);
 				}
 			}
 
-
 			let filteredColumnBoxes = columnBoxes.filter(box => box.value !== 0);
 
-			for (let j = 0; j < filteredColumnBoxes.length; j++) {
-				console.log(filteredColumnBoxes[j].value);
-			}
-
+			// for (let j = 0; j < filteredColumnBoxes.length; j++) {
+			// 	console.log(filteredColumnBoxes[j].value);
+			// }
 
 			for (let j = 0; j < filteredColumnBoxes.length - 1; j++) {
 				if (filteredColumnBoxes[j].value === filteredColumnBoxes[j+1].value) {
@@ -166,81 +108,37 @@ const Board = {
 				}
 			}
 
-
-			for(let i=filteredColumnBoxes.length;i<SIZE;i++) {
+			for (let i=filteredColumnBoxes.length;i<SIZE;i++) {
 				filteredColumnBoxes.push(this.getEmptyBox(0,0));
 			}
 
-
 			if (filteredColumnBoxes.length>0) {
-				if(direction === DIRECTION.UP){
+				if (direction === DIRECTION.UP) {
 					for (let j = 0; j < SIZE; j++) {
 						this._desk[s][j].value = filteredColumnBoxes[j].value;
 					}
 				}
-				if(direction === DIRECTION.DOWN){
+				if (direction === DIRECTION.DOWN) {
 					for (let j = SIZE-1; j >= 0; j--) {
 						this._desk[s][j].value = filteredColumnBoxes[SIZE-1-j].value;
 					}
 				}
-				if(direction === DIRECTION.LEFT){
+				if (direction === DIRECTION.LEFT) {
 					for (let j = 0; j < SIZE; j++) {
 						this._desk[j][s].value = filteredColumnBoxes[j].value;
 					}
 				}
-				if(direction === DIRECTION.RIGHT){
+				if (direction === DIRECTION.RIGHT) {
 					for (let j = SIZE-1; j >= 0; j--) {
 						this._desk[j][s].value = filteredColumnBoxes[SIZE-1-j].value;
 					}
 				}
 			}
 
-
-
-
-
-			// if (filteredColumnBoxes.length>0) {
-
-
-			// 	for (let j = 0; j < SIZE; j++) {
-			// 		console.log(filteredColumnBoxes);
-			// 		if(direction === DIRECTION.UP || direction === DIRECTION.DOWN){
-			// 			this._desk[s][j].value = filteredColumnBoxes[j].value;
-
-			// 			// if (j < filteredColumnBoxes.length) {
-			// 			// 	this._desk[s][j].value = filteredColumnBoxes[j].value; // fix
-			// 			// } else {
-
-			// 			// 	this._desk[s][j].value = 0; // fix
-			// 			// }
-
-			// 		}
-			// 		// else{
-			// 		// 	console.log(filteredColumnBoxes);
-			// 		// 	if (j < SIZE - filteredColumnBoxes.length) {
-			// 		// 		this._desk[s][j].value = 0;
-			// 		// 	} else {
-			// 		// 		console.log(j - (SIZE - filteredColumnBoxes.length));
-			// 		// 		console.log(filteredColumnBoxes[ j - (SIZE - filteredColumnBoxes.length) ].value);
-			// 		// 		this._desk[s][j].value = filteredColumnBoxes[ j - (SIZE - filteredColumnBoxes.length) ].value;// fix
-
-			// 		// 	}
-			// 		// }
-
-			// 	}
-			// }
-
-		// }
-		console.log(this._desk);
-	}
-
+			// console.log(this._desk);
+		}
 	}
 }
-
-
-
-
-
 
 const Draw = {
 	init: function () {
@@ -291,8 +189,11 @@ const Player = {
 	makeMove: function (direction) {
 		console.log(direction);
 
-
 		Board.mergeBoxes(direction);
+
+		if (!Board.isMoved()) {
+			return;
+		}
 		Board.generateBox(); // last
 		Draw.drawDesk();
 	},
